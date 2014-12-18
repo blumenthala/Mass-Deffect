425b33cf399ba58598ac0a2aeb7883f4c1f9a014
duggank <CreativeWeep@gmail.com>
2014-12-18 08:26:50 -0500

    MOvement close

--- MassEffectMap.cs/MassEffectMap.cs/MassEffectMap.cs/Normandy.cs
+++ MassEffectMap.cs/MassEffectMap.cs/MassEffectMap.cs/Normandy.cs
@@ -17,17 +17,39 @@ namespace MassEffectMap.cs
     /// </summary>
     public class Normandy
     {
-        Texture2D Mormandy;
+        Texture2D NormandyShip;
         Sprite norman;
-        private Normandy normandy;
+        private float playerSpeed = 160.0f;
+        private Rectangle playerAreaLimit;
+        Rectangle screenBounds;
+
+
         public Normandy(Texture2D text)
         {
-            Mormandy = text;
-            norman = new Sprite(new Vector2(300, 300), Mormandy, new Rectangle(0, 0, 194, 672), Vector2.Zero);
+            NormandyShip = text;
+            norman = new Sprite(new Vector2(300, 300), NormandyShip, new Rectangle(0, 0, 194, 672), Vector2.Zero);
         }
 
-      
+        public Normandy(
+            Texture2D texture,  
+            Rectangle initialFrame,
+            int frameCount,
+            Rectangle screenBounds)
+        {
+            norman = new Sprite(
+                new Vector2(500, 500),
+                texture,
+                initialFrame,
+                Vector2.Zero);
 
+           
+            playerAreaLimit =
+                new Rectangle(
+                    0,
+                    screenBounds.Height / 2,
+                    screenBounds.Width,
+                    screenBounds.Height / 2);
+        }
         private void HandleKeyboardInput(KeyboardState keyState)
         {
             if (keyState.IsKeyDown(Keys.Up))
@@ -51,6 +73,37 @@ namespace MassEffectMap.cs
             }
         }
 
+        private void HandleGamepadInput(GamePadState gamePadState)
+        {
+            norman.Velocity +=
+                new Vector2(
+                    gamePadState.ThumbSticks.Left.X,
+                    -gamePadState.ThumbSticks.Left.Y);
+
+        }
+
+        private void imposeMovementLimits()
+        {
+            Vector2 location = norman.Location;
+
+            if (location.X < playerAreaLimit.X)
+                location.X = playerAreaLimit.X;
+
+            if (location.X >
+                (playerAreaLimit.Right - norman.Source.Width))
+                location.X =
+                    (playerAreaLimit.Right - norman.Source.Width);
+
+            if (location.Y < playerAreaLimit.Y)
+                location.Y = playerAreaLimit.Y;
+
+            if (location.Y >
+                (playerAreaLimit.Bottom - norman.Source.Height))
+                location.Y =
+                    (playerAreaLimit.Bottom - norman.Source.Height);
+
+            norman.Location = location;
+        }
      
 
         /// </summary>
@@ -62,6 +115,18 @@ namespace MassEffectMap.cs
         {
 
             norman.Update(gameTime);
+
+            norman.Velocity = Vector2.Zero;
+
+            HandleKeyboardInput(Keyboard.GetState());
+            HandleGamepadInput(GamePad.GetState(PlayerIndex.One));
+
+            norman.Velocity.Normalize();
+            norman.Velocity *= playerSpeed;
+
+            norman.Update(gameTime);
+            imposeMovementLimits();
+
         }
     }
 }
